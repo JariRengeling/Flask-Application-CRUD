022c80991d0dd3a694128a7181c69fc2e150de4c
Jari Rengeling <jarirengeling@outlook.com>
2018-03-28 15:06:54 +0200

    Getting Started
    Flask | Getting Started

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut dolor vel lectus facilisis efficitur pretium a sapien. Suspendisse sodales eleifend tempor.',
+            'author': 'Jari Rengeling',
+            'create_date': '03-01-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut dolor vel lectus facilisis efficitur pretium a sapien. Suspendisse sodales eleifend tempor.',
+            'author': 'John Doe',
+            'create_date': '03-01-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut dolor vel lectus facilisis efficitur pretium a sapien. Suspendisse sodales eleifend tempor.',
+            'author': 'Brad Traversy',
+            'create_date': '03-01-2018'
+        }
+    ]
+    return articles
